fd80f5f4565f51ab9a3b34ffc2d03c0c11521007
Mohamed H <mohamed.hadjoudj@protonmail.com>
2018-10-14 16:13:53 +0200

    Jeu fonctionnel

--- fling/_build/rules.ml
+++ fling/_build/rules.ml
@@ -24,29 +24,84 @@ let get_balls g = g
 let is_ball g p =
   let rec aux = function
       [] -> false
-    | (p', i)::b -> if p=p' then true else aux b
+    | (p', i)::b -> if Position.eq p p' then true else aux b
   in
-  aux g
+  aux (get_balls g)
 
 let ball_of_position game p =
   let rec aux = function
       [] -> failwith "ball_of_position"
-    | (p', i)::b -> if p=p' then (p', i) else aux b
+    | (p', i)::b -> if Position.eq p p' then (p', i) else aux b
   in
   aux game
 
 let position_of_ball b = match b with (a, _) -> a
 
-let apply_move a b =
-  let  g,((p, i), d) = Obj.magic (a,b) in
-  let next_pos (x',y') = match d with Up -> (x',y'+1) | Right -> (x'+1, y') | Down -> (x', y'-1) | Left -> (x'-1, y') in
-  let pos_in_range (x', y') = 0 <= x' && 0<= y' && x' < 15 && y' < 15 in
+let apply_move g move =
+  (*Raccourcis*)
+  let px = Position.proj_x and py = Position.proj_y in
+  let pos = Position.from_int in
+
+  let (b, dir) = move in
+  let (p, id) = b in
+  let x,y = px p, py p in
+
+  let balls = get_balls g in
+
+  (*Creation de la liste des balles se trouvant dans la direction dir dans l'ordre*)
+  let selector b' = let (p', _) = b' in match dir with Up -> x = (px p') && y < (py p')
+    | Right -> y = (py p') && x < (px p')
+    | Down -> x = (px p') && y > (py p')
+    | Left -> y = (py p') && x > (px p') in
+  let ordre b b' = let (p, _), (p', _) = b, b' in match dir with Up -> (py p') - (py p)
+    | Right -> (px p') - (px p)
+    | Down -> (py p) - (py p')
+    | Left -> (px p) - (px p') in
+  let balls_dir = List.sort ordre (List.filter selector balls) in
+
+  let prev_pos p = let x = px p and y = py p in
+  match dir with Down -> pos x (y+1)
+    | Left -> pos (x+1) y
+    | Up -> pos x (y-1)
+    | Right -> pos (x-1) y in
+
+  (*Met à jour les positions des balles*)
+  let update_game g balls =
+      let rec update_ball l b = match l with [] -> b
+        | b'::q -> if eq_ball b' b then b' else update_ball q b
+      in List.map (update_ball balls) g
+  in
+
+  let remove_ball balls b =
+    List.filter (fun b' -> not (eq_ball b b')) balls in
+
+  (*Renvoi la liste des balles dont la position doit changer*)
+  let rec aux acc lst = function [] -> if eq_ball lst b then g else remove_ball (update_game g acc) lst
+    | (p', i')::q -> let (p, i) = lst in if i=id  && Position.eq p (prev_pos p') then g
+      else let acc' = ((prev_pos p'), i)::acc in
+        aux acc' (p', i') q in
+
+  aux [] b balls_dir
+
+  (*
+  let remove_ball balls b =
+    List.filter (fun b' -> not eq_ball b b') balls
+
+  let next_pos p = let x = px p and y = py p in
+    match dir with Up -> pos x (y+1)
+      | Right -> pos (x+1) y
+      | Down -> pos x (y-1)
+      | Left -> pos (x-1) y in
+
+  let balls_dir = List.fold_left ()
+
   let p' = next_pos p in
   if is_ball g p' then g
-  else let rec aux game i' p' =
+  else let rec aux balls i' p' =
          let p'' = next_pos p' in
          if not (pos_in_range p'') then if i=i' then g else List.filter (fun (_, k) -> k!=i) game
          else if is_ball game p'' then let (q, j) = ball_of_position game p'' in
            aux (List.map (fun (a,b) -> if b=i' then (p', i') else (a,b)) game) j (next_pos q)
-         else aux game i' p'' in Obj.magic (aux g i p')
+         else aux g i' p'' in Obj.magic (aux g i p')
 
+  *)
