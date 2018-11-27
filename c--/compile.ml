e9e606a9f4c3f4efb691c6a36cf236077e39c954
Mohiceage <mohiceage@pm.me>
2018-11-27 20:46:18 +0100

    poulain

--- c--/compile.ml
+++ c--/compile.ml
@@ -1,5 +1,29 @@
 open Cparse
 open Genlab
 
+
 let compile out decl_list =
-  Printf.fprintf out "TODO\n"
+  let todo s = Printf.fprintf out "#TODO %s\n" s
+  in
+  todo "test";
+ let rec compile_decl_list = function 
+|[] -> () 
+|(h::t) ->  compile_decl h; compile_decl_list t
+and compile_decl = function 
+ |CDECL(l,s) -> todo ("cdecl " ^ s)
+ |CFUN (l,s, vdl , lc) -> begin 
+ todo ("cfun "^s);
+ List.iter compile_decl vdl;
+ compile_code lc
+ end
+and compile_code (_,c) = match c with 
+|CBLOCK(vdl,lcl) -> begin 
+ todo ("cblock ");
+List.iter compile_decl vdl;
+List.iter compile_code lcl
+end
+|_ -> todo ("not cblock")
+in 
+compile_decl_list decl_list
+
+  
